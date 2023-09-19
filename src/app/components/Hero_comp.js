ed3b4c62a9b2acdc881d73ede459d0de47648339
Funmilola Adebisi <elizabeth.tamera@gmail.com>
2023-09-19 10:02:07 +0100

    initial commit

--- src/app/components/Hero_comp.js
+++ src/app/components/Hero_comp.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+// import image
+import PurpleLens from "../assets/img/PurpleLens.png"
+import Line1 from "../assets/img/Line1.png"
+
 const Hero_comp = () => {
   return (
-    <div>Hero Component</div>
-  )
+    <section className="section bg-gray-900">
+        <div className="">hero</div>
+    </section>
+  );
 }
 
 export default Hero_comp
